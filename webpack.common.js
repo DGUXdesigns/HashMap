4a1645ac127422ab666831760c6149363f5d2a33
Daveian <140139328+DGUXdesigns@users.noreply.github.com>
2025-01-08 18:31:16 -0500

    Configuration changes

--- webpack.common.js
+++ webpack.common.js
@@ -19,16 +19,8 @@ module.exports = {
   module: {
     rules: [
       {
-        test: /\.css$/i,
-        use: ['style-loader', 'css-loader'],
-      },
-      {
         test: /\.html$/i,
         loader: 'html-loader',
-      },
-      {
-        test: /\.(png|svg|jpg|jpeg|gif)$/i,
-        type: 'asset/resource',
       },
     ],
   },

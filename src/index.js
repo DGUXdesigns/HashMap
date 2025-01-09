ee95c954a45251f210fc53b4bba2345c842c8cc9
Daveian <140139328+DGUXdesigns@users.noreply.github.com>
2025-01-08 22:48:08 -0500

    Add test calls for Hashmap

--- src/index.js
+++ src/index.js
@@ -1,1 +1,25 @@
-import './style.css';
+import { HashMap } from './hashMap.js';
+
+const test = new HashMap(16, 0.75);
+
+// Set key, value
+test.set('apple', 'red');
+test.set('banana', 'yellow');
+test.set('carrot', 'orange');
+test.set('dog', 'brown');
+test.set('elephant', 'gray');
+test.set('frog', 'green');
+test.set('grape', 'purple');
+test.set('hat', 'black');
+test.set('ice cream', 'white');
+test.set('jacket', 'blue');
+test.set('kite', 'pink');
+test.set('lion', 'golden');
+
+console.table(test.buckets);
+console.log(test.length());
+
+test.set('moon', 'silver');
+
+console.table(test.buckets);
+console.log(test.buckets.length);

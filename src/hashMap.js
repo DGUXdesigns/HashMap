f6651e1782e3ac95e7f77eeaaa1137d8fad9bc9e
Daveian <140139328+DGUXdesigns@users.noreply.github.com>
2025-01-08 20:06:10 -0500

    Add `HashMap` class and functionality
    - Implemented `hash` function to turn keys into a hashcode
    - Added `set` to set key, value pairs to hashMap
    - Added `get` to return the value of a specified key within in hashMap

---
+++ src/hashMap.js
@@ -0,0 +1,45 @@
+export class HashMap {
+  constructor(capacity, loadFactor) {
+    this.buckets = new Array(capacity).fill().map(() => []);
+    this.loadFactor = loadFactor;
+  }
+
+  hash(key) {
+    let hashCode = 0;
+
+    const primeNumber = 31;
+    for (let i = 0; i < key.length; i++) {
+      hashCode =
+        (primeNumber * hashCode + key.charCodeAt(i)) % this.buckets.length;
+    }
+
+    return hashCode;
+  }
+
+  set(key, value) {
+    const bucketIndex = this.hash(key);
+    const bucket = this.buckets[bucketIndex];
+
+    for (let i = 0; i < bucket.length; i++) {
+      if (bucket[i].key === key) {
+        bucket[i].value = value;
+        return;
+      }
+    }
+
+    bucket.push({ key, value });
+  }
+
+  get(key) {
+    const bucketIndex = this.hash(key);
+    const bucket = this.buckets[bucketIndex];
+
+    for (let i = 0; i < bucket.length; i++) {
+      if (bucket[i].key === key) {
+        return bucket[i].value;
+      } else {
+        return null;
+      }
+    }
+  }
+}

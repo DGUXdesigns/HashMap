c4e83449047ca51358e1878977286a7b96624230
Daveian <140139328+DGUXdesigns@users.noreply.github.com>
2025-01-08 21:43:58 -0500

    Add methods to get all stored keys and values
    - `keys` to return all stored keys
    - `values` to return all stored values
    - 'entries' to return all stored key, value pairs

--- src/hashMap.js
+++ src/hashMap.js
@@ -83,4 +83,51 @@ export class HashMap {
   clear() {
     this.buckets = new Array(this.buckets.length).fill().map(() => []);
   }
+
+  keys() {
+    const keysArray = [];
+
+    for (let i = 0; i < this.buckets.length; i++) {
+      const bucket = this.buckets[i];
+
+      for (let j = 0; j < bucket.length; j++) {
+        keysArray.push(bucket[j].key);
+      }
+    }
+
+    return keysArray;
+  }
+
+  values() {
+    const valuesArray = [];
+
+    for (let i = 0; i < this.buckets.length; i++) {
+      const bucket = this.buckets[i];
+
+      for (let j = 0; j < bucket.length; j++) {
+        valuesArray.push(bucket[j].value);
+      }
+    }
+
+    return valuesArray;
+  }
+
+  entries() {
+    const entriesArray = [];
+
+    for (let i = 0; i < this.buckets.length; i++) {
+      const bucket = this.buckets[i];
+
+      for (let j = 0; j < bucket.length; j++) {
+        const newArr = [];
+
+        newArr.push(bucket[j].key);
+        newArr.push(bucket[j].value);
+
+        entriesArray.push(newArr);
+      }
+    }
+
+    return entriesArray;
+  }
 }
